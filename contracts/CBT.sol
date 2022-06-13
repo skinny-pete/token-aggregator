b19004fc0567f4aaf38f5c4178f9430e808c4272
theodale <theomjdale@gmail.com>
2022-06-13 10:27:39 +0100

    tests and repo redo

--- contracts/CBT.sol
+++ contracts/CBT.sol
@@ -4,13 +4,27 @@ pragma solidity >=0.8.0;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract CBT is ERC20, Ownable {
+/// @title Climate Base Tonne
+/// @author Theo Dale & Peter Whitby
+/// @notice A token that represents unretired climate assets
+contract ClimateBackedTonne is ERC20, Ownable {
+    /// @dev Follow CBT naming conventions
+    /// @param name Name of CBT
+    /// @param symbol Symbol of CBT
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
+    /// @notice Mint an amount of CBT to a users wallet
+    /// @dev Contract owner only
+    /// @param who Recipient of fresh CBT
+    /// @param amount Amount of CBT that will be minted
     function mint(address who, uint256 amount) public onlyOwner {
         _mint(who, amount);
     }
 
+    /// @notice Burn an amount of CBT from a users wallet
+    /// @dev Contract owner only
+    /// @param who Address who's CBT will be burned
+    /// @param amount Amount of CBT that will be burned
     function burn(address who, uint256 amount) public onlyOwner {
         _burn(who, amount);
     }
--- test/CBLK.js
+++ test/CBLK.js
@@ -1,79 +1,346 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
+const { mintAndApprove } = require('./utils');
+
 describe('CBLK', () => {
   let CBLKFactory, CBTFactory; // Contract factories
-  let CBLK, CBTs; // Contract instances
+  let CBLK, CBT1, CBT2, CBT3; // Contract instances
   let deployer, notDeployer; // Signers
-  let CBTAddresses; // Convenince values
 
   before(async () => {
-    CBLKFactory = await ethers.getContractFactory('CBLK');
-    CBTFactory = await ethers.getContractFactory('CBT');
     [deployer, notDeployer] = await ethers.getSigners();
+    CBTFactory = await ethers.getContractFactory('ClimateBackedTonne');
+    CBLKFactory = await ethers.getContractFactory('Changeblock');
   });
 
   beforeEach(async () => {
+    // Setup CBLK with 1000 of each CBT
     CBLK = await CBLKFactory.deploy('Changeblock', 'CBLK');
-    CBTs = [
-      await CBTFactory.deploy('Carbon Base Tonne: 1', 'CBT-1'),
-      await CBTFactory.deploy('Carbon Base Tonne: 2', 'CBT-2'),
-      await CBTFactory.deploy('Carbon Base Tonne: 3', 'CBT-3'),
-    ];
-    CBTAddresses = CBTs.map((CBT) => CBT.address);
+    CBT1 = await CBTFactory.deploy('Carbon Base Tonne: 1', 'CBT1');
+    CBT2 = await CBTFactory.deploy('Carbon Base Tonne: 2', 'CBT2');
+    CBT3 = await CBTFactory.deploy('Carbon Base Tonne: 3', 'CBT3');
+    await mintAndApprove(
+      [CBT1, CBT2, CBT3],
+      [
+        ethers.utils.parseEther('1000'),
+        ethers.utils.parseEther('1000'),
+        ethers.utils.parseEther('1000'),
+      ],
+      deployer,
+      CBLK
+    );
+    await CBLK.rebalance(
+      [CBT1.address, CBT2.address, CBT3.address],
+      [
+        ethers.utils.parseEther('1000'),
+        ethers.utils.parseEther('1000'),
+        ethers.utils.parseEther('1000'),
+      ],
+      [],
+      []
+    );
   });
 
-  // Mint to deployer and approve to a recipient
-  const mintAndApprove = async (tokens, amounts, owner, spender) => {
-    for (let i = 0; i < tokens.length; i++) {
-      await tokens[i].mint(owner.address, amounts[i]);
-      await tokens[i].approve(spender.address, amounts[i]);
-    }
-  };
-
-  describe('Deposits', async () => {
-    const CBTDeposits = ['100', '200', '300'].map((n) =>
-      ethers.utils.parseEther(n)
-    );
-
-    it('Correct post-deposit token balances', async () => {
-      await mintAndApprove(CBTs, CBTDeposits, deployer, CBLK);
-      await CBLK.deposit(CBTAddresses, CBTDeposits);
-      await expect(await CBLK.balanceOf(deployer.address)).to.equal(
-        ethers.utils.parseEther('600')
+  describe('Rebalance', async () => {
+    it('Correct state post inital CBT deposits', async () => {
+      expect(await CBT1.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('1000')
       );
-      for (let i = 0; i < CBTs.length; i++) {
-        await expect(await CBTs[i].balanceOf(CBLK.address)).to.equal(
-          CBTDeposits[i]
+      expect(await CBT2.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('1000')
+      );
+      expect(await CBT3.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('1000')
+      );
+      expect(await CBLK.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('3000')
+      );
+      expect(await CBT1.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('0')
+      );
+      expect(await CBT2.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('0')
+      );
+      expect(await CBT3.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('0')
+      );
+      expect(await CBLK.climateBackedTonnes(0)).to.equal(CBT1.address);
+      expect(await CBLK.climateBackedTonnes(1)).to.equal(CBT2.address);
+      expect(await CBLK.climateBackedTonnes(2)).to.equal(CBT3.address);
+    });
+
+    it('Emits `Rebalance` event ', async () => {
+      await mintAndApprove(
+        [CBT2],
+        [ethers.utils.parseEther('250')],
+        deployer,
+        CBLK
+      );
+      await expect(
+        CBLK.rebalance(
+          [CBT2.address],
+          [ethers.utils.parseEther('250')],
+          [CBT1.address, CBT3.address],
+          [ethers.utils.parseEther('100'), ethers.utils.parseEther('100')]
+        )
+      )
+        .to.emit(CBLK, 'Rebalance')
+        .withArgs(
+          [CBT2.address],
+          [ethers.utils.parseEther('250')],
+          [CBT1.address, CBT3.address],
+          [ethers.utils.parseEther('100'), ethers.utils.parseEther('100')]
         );
-      }
     });
 
-    it('Emits `Deposit` event', async () => {
-      await mintAndApprove(CBTs, CBTDeposits, deployer, CBLK);
-      await expect(CBLK.deposit(CBTAddresses, CBTDeposits))
-        .to.emit(CBLK, 'Deposit')
-        .withArgs(CBTAddresses, CBTDeposits);
+    it('Rebalance with equal input and output', async () => {
+      await mintAndApprove(
+        [CBT2],
+        [ethers.utils.parseEther('100')],
+        deployer,
+        CBLK
+      );
+      await CBLK.rebalance(
+        [CBT2.address],
+        [ethers.utils.parseEther('100')],
+        [CBT1.address, CBT3.address],
+        [ethers.utils.parseEther('50'), ethers.utils.parseEther('50')]
+      );
+      expect(await CBT1.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('950')
+      );
+      expect(await CBT2.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('1100')
+      );
+      expect(await CBT3.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('950')
+      );
+      expect(await CBLK.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('3000')
+      );
+      expect(await CBT1.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('50')
+      );
+      expect(await CBT2.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('0')
+      );
+      expect(await CBT3.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('50')
+      );
     });
 
-    it('Rejects non-owner deposits', async () => {
-      await mintAndApprove(CBTs, CBTDeposits, notDeployer, CBLK);
+    it('Requires CBLK burn when output > input', async () => {
+      await mintAndApprove(
+        [CBT2],
+        [ethers.utils.parseEther('100')],
+        deployer,
+        CBLK
+      );
+      await CBLK.rebalance(
+        [CBT2.address],
+        [ethers.utils.parseEther('100')],
+        [CBT1.address, CBT3.address],
+        [ethers.utils.parseEther('100'), ethers.utils.parseEther('100')]
+      );
+      expect(await CBT1.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('900')
+      );
+      expect(await CBT2.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('1100')
+      );
+      expect(await CBT3.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('900')
+      );
+      expect(await CBLK.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('2900')
+      );
+      expect(await CBT1.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('100')
+      );
+      expect(await CBT2.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('0')
+      );
+      expect(await CBT3.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('100')
+      );
+    });
+
+    it('Mints CBLK when input > output', async () => {
+      await mintAndApprove(
+        [CBT2],
+        [ethers.utils.parseEther('500')],
+        deployer,
+        CBLK
+      );
+      await CBLK.rebalance(
+        [CBT2.address],
+        [ethers.utils.parseEther('500')],
+        [CBT1.address, CBT3.address],
+        [ethers.utils.parseEther('100'), ethers.utils.parseEther('100')]
+      );
+      expect(await CBT1.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('900')
+      );
+      expect(await CBT2.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('1500')
+      );
+      expect(await CBT3.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('900')
+      );
+      expect(await CBLK.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('3300')
+      );
+      expect(await CBT1.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('100')
+      );
+      expect(await CBT2.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('0')
+      );
+      expect(await CBT3.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('100')
+      );
+    });
+
+    it('Requires sufficient CBT approval ', async () => {
       await expect(
-        CBLK.connect(notDeployer).deposit(CBTAddresses, CBTDeposits)
-      ).to.be.revertedWith('Ownable: caller is not the owner');
+        CBLK.rebalance(
+          [CBT2.address],
+          [ethers.utils.parseEther('500')],
+          [CBT1.address, CBT3.address],
+          [ethers.utils.parseEther('100'), ethers.utils.parseEther('100')]
+        )
+      ).to.be.reverted;
     });
 
-    it('Rejects deposits with insufficient approved CBTs', async () => {
-      await mintAndApprove(CBTs, CBTDeposits, deployer, CBLK);
-      const increasedCBTDeposits = CBTDeposits.map((amount) => amount.mul('2'));
+    it('Introduction of new token', async () => {
+      const CBT4 = await CBTFactory.deploy('Carbon Base Tonne: 4', 'CBT4');
+      await mintAndApprove(
+        [CBT4],
+        [ethers.utils.parseEther('1000')],
+        deployer,
+        CBLK
+      );
+      await CBLK.rebalance(
+        [CBT4.address],
+        [ethers.utils.parseEther('1000')],
+        [],
+        []
+      );
+      expect(await CBT4.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('1000')
+      );
+      expect(await CBLK.climateBackedTonnes(3)).to.equal(CBT4.address);
+    });
+
+    it('De-registers token on removal', async () => {
+      await CBLK.rebalance(
+        [],
+        [],
+        [CBT1.address],
+        [ethers.utils.parseEther('1000')]
+      );
+      expect(await CBLK.climateBackedTonnes(0)).to.equal(CBT3.address);
+      expect(await CBLK.climateBackedTonnes(1)).to.equal(CBT2.address);
+      await expect(CBLK.climateBackedTonnes(2)).to.be.reverted;
+    });
+  });
+
+  describe('Withdraw', () => {
+    it('Complete withdrawal', async () => {
+      await expect(CBLK.withdraw(ethers.utils.parseEther('3000')));
+      expect(await CBT1.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('0')
+      );
+      expect(await CBT2.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('0')
+      );
+      expect(await CBT3.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('0')
+      );
+      expect(await CBLK.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('0')
+      );
+      expect(await CBT1.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('1000')
+      );
+      expect(await CBT2.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('1000')
+      );
+      expect(await CBT3.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('1000')
+      );
+      await expect(CBLK.climateBackedTonnes(0)).to.be.reverted;
+    });
+
+    // Check dust amounts are as understood
+    it('Partial withdrawal', async () => {
+      await CBLK.withdraw(ethers.utils.parseEther('1250'));
+      expect(await CBT1.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('1250').mul('1000').div('3000')
+      );
+      expect(await CBT2.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('1250').mul('1000').div('3000')
+      );
+      expect(await CBT3.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('1250').mul('1000').div('3000')
+      );
+      expect(await CBLK.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('1750')
+      );
+      expect(await CBLK.totalSupply()).to.equal(
+        ethers.utils.parseEther('1750')
+      );
+    });
+    it('Emits `Withdrawal` event ', async () => {
+      await expect(CBLK.withdraw(ethers.utils.parseEther('1000')))
+        .to.emit(CBLK, 'Withdrawal')
+        .withArgs(deployer.address, [
+          ethers.utils.parseEther('1000').mul('1000').div('3000'),
+          ethers.utils.parseEther('1000').mul('1000').div('3000'),
+          ethers.utils.parseEther('1000').mul('1000').div('3000'),
+        ]);
+    });
+
+    it('Rejects withdrawal with insufficient CBLK', async () => {
       await expect(
-        CBLK.deposit(CBTAddresses, increasedCBTDeposits)
-      ).to.be.revertedWith('ERC20: insufficient allowance');
+        CBLK.connect(notDeployer).withdraw(ethers.utils.parseEther('1000'))
+      ).to.be.revertedWith('ERC20: burn amount exceeds balance');
+      await expect(
+        CBLK.withdraw(ethers.utils.parseEther('4000'))
+      ).to.be.revertedWith('ERC20: burn amount exceeds balance');
+    });
+
+    it('De-registers tokens on removal', async () => {
+      await CBLK.withdraw(ethers.utils.parseEther('3000'));
+      await expect(CBLK.climateBackedTonnes(0)).to.be.reverted;
+    });
+
+    it('Dust is collected by last withdrawer', async () => {
+      await CBLK.withdraw(ethers.utils.parseEther('1234'));
+      await CBLK.withdraw(ethers.utils.parseEther('1766'));
+      expect(await CBLK.totalSupply()).to.equal('0');
+      expect(await CBT1.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('0')
+      );
+      expect(await CBT2.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('0')
+      );
+      expect(await CBT3.balanceOf(CBLK.address)).to.equal(
+        ethers.utils.parseEther('0')
+      );
+      expect(await CBT1.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('1000')
+      );
+      expect(await CBT2.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('1000')
+      );
+      expect(await CBT3.balanceOf(deployer.address)).to.equal(
+        ethers.utils.parseEther('1000')
+      );
+      await expect(CBLK.climateBackedTonnes(0)).to.be.reverted;
     });
   });
 });
 
-// We want proof it functions as it's supposed to,
-// so allows assembly and disassembly and check it's the right number minted and burned and returned
-// Check that any whitelisting or anything like that works
-// Check total supply is updated etc
+// npx hardhat test
