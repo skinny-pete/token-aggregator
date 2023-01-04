2a4c1cc73d4767023584223aa607eb002c37318d
Peter W <pwhitby8@gmail.com>
2023-01-04 14:51:05 +0000

    initial

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# CBT and CBLK
+# CBT / CBLK
 
-This repo contains the contracts and unit tests for Changeblock's CBT and CBLK
-
-Authors - Theo Dale & Peter Whitby
-
-Learn more @ https://www.changeblock.com/
+## A token aggregation protocol similar to an index
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,8 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 require('hardhat-contract-sizer');
 
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.4',
   settings: {
     optimizer: {
       enabled: true,
@@ -12,12 +12,6 @@ module.exports = {
   networks: {
     hardhat: {
       allowUnlimitedContractSize: true,
-    },
-    mumbai: {
-      url: "https://speedy-nodes-nyc.moralis.io/614505f786fface05285bafb/polygon/mumbai",
-      accounts: [
-        "c22876839be9bb295ca99f427c53dc76c1f960a4f7e428a5e4d2ed7c62feb204",
-      ],
     },
   },
 };
